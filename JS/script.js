6395fb70aa9a1276deb617c2cfccacb7e52a7916
Pruvot Fabian <pruvotfabian@gmail.com>
2018-08-03 09:54:07 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,120 @@
+//alert("Hey!");
+
+// Version Long //
+
+//document.getElementById("titre").innerHTML="Mon beau titre";
+//document.getElementById("titre").style.display="none";
+
+// Version optimiser //
+
+    //var banana= document.getElementById("titre");
+            
+                 var poire= document.getElementById("poire");
+                 var meteo = prompt("Quel temps fait-il ?");
+               switch (meteo){
+                   
+                   case 'soleil':
+                   poire.innerHTML = "Sa pique !";
+                   break;
+                   case 'pluie':
+                   poire.innerHTML = "Sa mouille!";
+                   break;
+                   case 'vent':
+                   poire.innerHTML = "Sa souffle!";
+                   break;
+                   case 'neige':
+                   poire.innerHTML = "Gla Gla!";
+                   break;
+
+                default:
+                poire.innerHTML = "Pas compris";
+               }
+
+
+
+
+
+
+            // var meteo = prompt("Quel temps fait-il ?");
+            // if (meteo === "soleil"){
+            //     poire.innerHTML = "Sa pique !";
+            //     poire.style.color = "orange";
+            // }
+            // else if( meteo === "pluie")
+            // { 
+            //     poire.innerHTML = "Bienvenue à Calais";
+            //     poire.style.color = "grey";
+            // }
+            // else if( meteo === "neige")
+            // { 
+            //     poire.innerHTML = "Bienvenue au Canada";
+            //     poire.style.color = "grey";
+            // }
+            // else {
+            //     poire.innerHTML= "Pas compris";
+            // }
+
+
+
+
+        // var i = prompt("choisis un nombre : ");
+        // if (i>0 && i<=100){
+        //     console.log("i est positif");
+        //     poire.innerHTML = i + " est  compris entre 0 et 100";
+        //     poire.style.color = "green";
+        // }  
+        // else if( i>= 100){
+        //     console.log("i est négative");
+        //     poire.innerHTML = i + " est plus grand que 100";
+        //     poire.style.color = "red";
+        // }
+        // else if(i==0){
+        //     console.log("i est neutre");
+        //     poire.innerHTML = i + " Pauvre Type !";
+        //     poire.style.color = "grey";
+
+        // }
+        // else if(i<0){
+        //     console.log("i est négative");
+        //     poire.innerHTML = i + " est négatif";
+        //     poire.style.color = "red";
+        // }
+        // else{
+        //     console.log(" i n'est pas un nombre")
+        // }
+    //var city= document.getElementById("city");
+
+    //var nom = prompt("Ton Nom Bouffon !!!");
+    //console.log(nom);
+
+// Banana = ma variable //
+// Mon Beau Titre = Ce qui s'affiche //
+
+    //banana.innerHTML="Charlotte aux Fraises";
+
+    //banana.style.color= "whitesmoke";
+
+//******************************************************************/
+
+    //console.log("j'affiche tout et n'importe quoi dans la console");
+
+//var coco= 1 + "Salade de fruits";
+//var coco= 1 + 3;
+//var coco = "nom" + "prenom";
+//var coco = 5 / 10;
+
+// Pour Faire un espace entre les mots
+
+//var nom= "Pruvot";
+        //  \<-- Permet d'obliger Js d'affiche le texte même si se sont des ""
+//var prenom = "Fabian \"SHDHHD\" ";
+//var coco = "Bonjour " + nom + " " + prenom;
+//poire.innerHTML= coco;
+
+
+
+    //var array =["Fabian", "Pruvot", "Calais", "20 ans"];
+//poire.innerHTML= array[0];
+
+    //poire.innerHTML= "Prenom: " + array[0] + " Nom: " + array[1];
+    //city.innerHTML= " Ville: " + array[2] + " Age: " + array[3];
